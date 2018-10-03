3bfa4ffd5b5c97e2f0f07824b034e241b4b20770
UpdaterFile <Verhaegen8t@hotmail.com>
2018-10-03 14:57:48 -0700

    Initial commit

---
+++ app/src/main/java/com/cosc341project19/project19/MainActivity.java
@@ -0,0 +1,13 @@
+package com.cosc341project19.project19;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
